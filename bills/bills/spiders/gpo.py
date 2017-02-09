9dc82dcc7e92f772d47d20943f0cca6aa0e85bbe
bryce <thomsenbryce@gmail.com>
2017-02-08 21:39:01 -0600

    gather and follow links

--- bills/bills/spiders/gpo.py
+++ bills/bills/spiders/gpo.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
 
 class GpoSpider(scrapy.Spider):
     name = "gpo"
-    allowed_domains = ["https://www.gpo.gov/fdsys/bulkdata"]
-    #start_urls = ['http://https://www.gpo.gov/fdsys/bulkdata/']
+    #allowed_domains = ["https://www.gpo.gov/fdsys/bulkdata"]
     start_urls = [
-        'https://www.gpo.gov/fdsys/bulkdata/BILLSTATUS/115/sres',
+        'https://www.gpo.gov/fdsys/bulkdata',
     ]
+
+    #rules = (
+    #    Rule(LinkExtractor(allow=('https://www.gpo.gov/fdsys/bulkdata/*')),callback='parse')
+    #)
 
 
     def parse(self, response):
@@ -19,8 +24,12 @@ class GpoSpider(scrapy.Spider):
                     'date': page.xpath('.//td[2]/text()').extract_first(),
                     'size': page.xpath('.//td[3]/text()').extract_first(),
                 }
-
-        next_page = response.css('li.next a::attr(href)').extract_first()
-        if next_page is not None:
-            next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, callback=self.parse)
+            else:
+                next_link = page.xpath('.//td[1]/a[not(contains(@href, "zip"))]/@href').extract_first()
+                # gotta get rid of html, zip, pdf, xsl, and other junk
+                if next_link is not None:
+                    yield {
+                        'link': next_link
+                    }
+                    next_link = response.urljoin(next_link)
+                    yield scrapy.Request(next_link, callback=self.parse)
